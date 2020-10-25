9f061e64cb8684b531d243031652630fc3ef6945
zhz364 <zhz364@ucsd.edu>
2020-10-24 17:52:03 -0700

    Login user route and setup key

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const keys = require('../../confg/keys')
 
 router.get("/test", (req,res) =>{
     res.json({ msg: "This is the user route"});
@@ -31,6 +32,25 @@ router.post('/register', (req,res)=>{
             })
         }
     })
+})
+
+router.post('/login', (req, res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({email: "This user does not exist."})
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch =>{
+                    if(isMatch){
+                        res.json({msg: "Success!"})
+                    }else{
+                        return res.status(400).json({password: "Incorrect password"});
+                    }
+                })
+        })
 })
 
 module.exports = router;
