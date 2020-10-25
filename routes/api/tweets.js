21c35093b81f5acde9a62eb6685bf5b0e9d6a9a0
zhz364 <zhz364@ucsd.edu>
2020-10-24 23:10:07 -0700

    Set up tweet routes

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -8,6 +8,29 @@ router.get("/test", (req,res) =>{
     res.json({ msg: "This is the tweets route"});
 })
 
+router.get("/", (req,res) =>{
+    Tweet.find()
+        .sort({date: -1})
+        .then(tweets =>res.json(tweets))
+        .catch(err => res.status(400).json(err))
+})
+
+router.get("/user/:userId", (req,res)=>{
+    Tweet.find({user: req.params.userId})
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(400).json(err))
+
+})
+
+router.get('/:id', (req, res) => {
+    Tweet.findById(req.params.id)
+        .then(tweet => res.json(tweet))
+        .catch(err =>
+            res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+        )
+})
+
+
 router.post("/",
     passport.authenticate("jwt", {session:false}),
     (req,res) => {
