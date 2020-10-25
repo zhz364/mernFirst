f8055646f436e8f631619e59b5f7169e069e81e6
zhz364 <zhz364@ucsd.edu>
2020-10-24 21:58:04 -0700

    Set up validation for tweet

---
+++ validation/tweets.js
@@ -0,0 +1,22 @@
+const { json } = require("body-parser");
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+module.exports = function ValidateTweetInput(data){
+    let errors = {};
+
+    data.text = validText(data.text) ? data.text : "";
+
+    if(!Validator.isLength(data.text, {min: 5, max: 140})){
+        errors.text = "Tweet must be between 5 & 140 chars"
+    }
+    if(!Validator.isEmpty(data.text)){
+        errors.text = "Text field is required"
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
+    
+}
